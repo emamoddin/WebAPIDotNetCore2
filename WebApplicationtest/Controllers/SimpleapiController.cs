df9c76cd09d115121431074a20edeef622c31e04
emon-PC\emon <emam9447@gmail.com>
2018-03-11 00:36:05 +1100

    Initial Project Skeleton.

---
+++ WebApplicationtest/Controllers/SimpleapiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationtest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/API")]
+    public class SimpleapiController : Controller
+    {
+
+    public string[] Get()
+    {
+        return new string[]
+        {
+        "Hello",
+        "World"
+        };
+    }
+
+}
+}
